c658b6833bbd2310429637dcb4297cb094a963ba
Az1tchhh <azamatbazarbai007@gmail.com>
2022-03-18 18:16:02 +0600

    lab6 modified

--- lab6/dir/1.py
+++ lab6/dir/1.py
@@ -1,5 +1,16 @@
 import os
 w_d = os.getcwd()
+def show_content_files(path):
+    for item in os.listdir(path):
+        target_path = os.path.join(path, item)
+        if os.path.isfile(target_path):
+            print(item)
+def show_content_dirs(path):
+    for item in os.listdir(path):
+        target_path = os.path.join(path,item)
+        if os.path.isdir(target_path):
+            print(item)
+
 def show_content(path , d):
     for item in os.listdir(path):
         target_path = os.path.join(path, item)
@@ -9,5 +20,12 @@ def show_content(path , d):
             print('  '*d, "Dir : {0}".format(item))
             show_content(target_path, d+1)
 str = input("Input the main_path: ")
+path1 = os.path.join(w_d,str)
 str2 = input("Input the path in main_path: ")
-show_content(os.path.join( os.path.join(w_d, str) , str2) , 1)
+path2 = os.path.join(path1, str2)
+print("FILES:")
+show_content_files(path2)
+print("DIRS:")
+show_content_dirs(path2)
+print("DIRS and FILES:")
+show_content(path2 , 1)
