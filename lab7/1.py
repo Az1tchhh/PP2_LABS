3aecf5dbf677d6c04882fca8232386648ef55a71
Az1tchhh <azamatbazarbai007@gmail.com>
2022-03-28 21:11:17 +0600

    lab7

--- lab7/1.py
+++ lab7/1.py
@@ -1,14 +1,14 @@
+from time import time
 import pygame
 from datetime import datetime, timedelta
 import math
+import time
 
-RES = WIDTH, HEIGHT = 700, 618
-H_WIDTH, H_HEIGHT = WIDTH // 2, HEIGHT // 2
-RADIUS = H_HEIGHT - 50
-radius_list = {'sec': RADIUS - 20, 'min': RADIUS - 75, 'hour': RADIUS - 120}
+res = w, h = 700, 618
+w_half, h_half = w // 2, h // 2
 
 pygame.init()
-surface = pygame.display.set_mode(RES)
+surface = pygame.display.set_mode(res)
 clock = pygame.time.Clock()
 
 clock60 = dict(zip(range(60), range(0, 360, 6)))  # for hours, minutes and seconds
@@ -16,50 +16,35 @@ clock60 = dict(zip(range(60), range(0, 360, 6)))  # for hours, minutes and secon
 img = pygame.image.load('mickeyclock.png')
 img1 = pygame.image.load('longhandpng.png')
 img2 = pygame.image.load('shorthandpng.png')
-
-def get_clock_pos(clock_dict, clock_hand, key):
-    x = H_WIDTH + radius_list[key] * math.cos(math.radians(clock_dict[clock_hand]) - math.pi / 2)
-    y = H_HEIGHT + radius_list[key] * math.sin(math.radians(clock_dict[clock_hand]) - math.pi / 2)
-    img12 = pygame.transform.rotate(img1 , a)
-    surface.blit(img12, (mx - int(img12.get_width() / 2), my - int(img12.get_height() / 2)))
-    return x, y
-
-
+def get_clock_pos_seconds(clock_dict, second):
+    x = w_half + 200 * math.cos(math.radians(clock_dict[second]) - math.pi/2)
+    y = h_half + 200 * math.sin(math.radians(clock_dict[second]) - math.pi/2)
+    return x,y
+def get_clock_pos_minute(clock_dict, minute, key):
+    img12 = pygame.transform.rotate(img1 , -(clock_dict[minute]-90))    
+    surface.blit(img12, (w_half - int(img12.get_width() / 2), h_half - int(img12.get_height() / 2)))
 
 
-def longhand(time, clock_dict, clock_hand,a):
-    if clock_dict[clock_hand] == time.hour:
-        img12 = pygame.transform.rotate(img1 , a)
-        surface.blit(img12, (mx - int(img12.get_width() / 2), my - int(img12.get_height() / 2)))
-
-b = datetime.now().minute
-if b>15:
-    img12 = pygame.transform.rotate(img1 , (b-15)*6)
-    
-a=0
-cnt=0
+def get_clock_pos_hour(clock_dict, hour, key):
+    img12 = pygame.transform.rotate(img2 , -(clock_dict[hour]-270))    
+    surface.blit(img12, (w_half - int(img12.get_width() / 2), h_half - int(img12.get_height() / 2)))
+######crash##########
 while True:
-    a-=6
-    mx, my = H_WIDTH, H_HEIGHT
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             exit()
-    # set bg
-    # get time now
+#####################
+    surface.blit(img, (0, 0))
     t = datetime.now()
     hour, minute, second = ((t.hour % 12) * 5 + t.minute // 12) % 60, t.minute, t.second
-    # draw face
-    
-    surface.blit(img, (0, 0))
-    # draw clock
-    
-    img12 = pygame.transform.rotate(img1 , a)
-    surface.blit(img12, (mx - int(img12.get_width() / 2), my - int(img12.get_height() / 2)))
-    
-    pygame.draw.line(surface, pygame.Color('orange'), (H_WIDTH, H_HEIGHT), get_clock_pos(clock60, hour, 'hour'), 12)
-    pygame.draw.line(surface, pygame.Color('green'), (H_WIDTH, H_HEIGHT), get_clock_pos(clock60, minute, 'min'), 7)
+    pygame.draw.line(surface, 'black', (w_half, h_half), get_clock_pos_seconds(clock60, second), 7)
+    get_clock_pos_hour(clock60, hour, 'hour')
+    get_clock_pos_minute(clock60, minute, 'min')
 
-
+###############TEXT##################TEXT##########TEXT###############
+    basicFontForText = pygame.font.SysFont(None, 30)
+    text = basicFontForText.render('B Azamat', True, 'grey', 'white')
+    surface.blit(text, (0,0))
+########################################################################
     pygame.display.flip()
-    clock.tick(1)
+    clock.tick(60)
